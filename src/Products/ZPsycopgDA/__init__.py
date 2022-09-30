3478553f67b58552e78ce0f593c2790ec5f33d7b
Michael Howitz <mh@gocept.com>
2022-09-30 09:43:47 +0200

    Fix MANIFEST.

--- src/Products/ZPsycopgDA/__init__.py
+++ src/Products/ZPsycopgDA/__init__.py
@@ -16,7 +16,7 @@
 # their work without bothering about the module dependencies.
 
 __doc__ = "ZPsycopg Database Adapter Registration."
-__version__ = "2.5.0+rl0"
+__version__ = "2.5.0+rl1"
 
 import DA
 
