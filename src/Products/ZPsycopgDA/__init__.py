ea85af7605ccb575aa86320e7e322ccedb732946
Sebastian Wehrmann <sebastian@risclog.com>
2022-09-30 09:05:58 +0200

    Prerelease

--- src/Products/ZPsycopgDA/__init__.py
+++ src/Products/ZPsycopgDA/__init__.py
@@ -16,7 +16,7 @@
 # their work without bothering about the module dependencies.
 
 __doc__ = "ZPsycopg Database Adapter Registration."
-__version__ = "2.4.6-rl"
+__version__ = "2.5-rl"
 
 import DA
 
